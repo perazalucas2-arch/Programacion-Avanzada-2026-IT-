bc925304ad27634c78598ae725a10cd4dd5eb5cd
perazalucas2-arch <perazalucas2@gmail.com>
2026-04-26 21:20:32 -0300

    Update Main.java

--- pruebamatias/src/Main.java
+++ pruebamatias/src/Main.java
@@ -1,51 +1,15 @@
-import java.sql.SQLOutput;
-
 public class Main {
 
     public static void main(String[] args) {
 
-        Estudiante e1 = new Estudiante("juan", 30, "2", 10);
-        Estudiante e2 = new Estudiante("maria", 22, "1", 8);
-
-        System.out.println(e1.promedio);
-        e1.setPromedio(9);
-
-        System.out.println(e1.edad);
-        System.out.println(e2.nombre);
-        System.out.println(e1.promedio);
-
-    }
-
-    public static class Estudiante {
-        String nombre;
-        int edad;
-        String grupo;
-        int promedio;
-
-        public Estudiante(String nombre, int edad, String grupo, int promedio) {
-            this.nombre = nombre;
-            this.edad = edad;
-            this.grupo = grupo;
-            this.promedio = promedio;
+        Estudiante e1 = new Estudiante("maria", 18, 1, 10);
+        Estudiante e2 = new Estudiante("pepe", 20, 2, 4);
+        e1.mostrarDatos();
+        if (e1.getPromedio() > 5){
+            System.out.println("aprueba");
         }
-
-        public void setPromedio(int promedio) {
-            this.promedio = promedio;
-        }
-
-        public void setEdad(int edad) {
-            this.edad = edad;
-        }
-
-        public void setGrupo(String grupo) {
-            this.grupo = grupo;
-        }
-
-        public void setNombre(String nombre) {
-            this.nombre = nombre;
-
-
-
-        }
+        e1.setPromedio(12);
+        System.out.println(e1.aprueba());
+        System.out.println(e2.aprueba());
     }
 }
